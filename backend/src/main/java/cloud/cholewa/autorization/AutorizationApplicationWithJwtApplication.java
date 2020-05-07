79698d07dd01db305c7d1d9bf4399acfb8e24734
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-05-07 23:33:16 +0200

    chore: backend initial commit

---
+++ backend/src/main/java/cloud/cholewa/autorization/AutorizationApplicationWithJwtApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.autorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorizationApplicationWithJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutorizationApplicationWithJwtApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cloud/cholewa/autorization/AutorizationApplicationWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.autorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizationApplicationWithJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
