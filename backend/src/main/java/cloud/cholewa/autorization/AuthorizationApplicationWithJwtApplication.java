a1b62af86ba77ee5426317337339e6560b06f951
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-05-08 19:34:41 +0200

    chore: fixed typo and added swagger

--- backend/src/main/java/cloud/cholewa/autorization/AuthorizationApplicationWithJwtApplication.java
+++ backend/src/main/java/cloud/cholewa/autorization/AuthorizationApplicationWithJwtApplication.java
@@ -2,12 +2,14 @@ package cloud.cholewa.autorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class AutorizationApplicationWithJwtApplication {
+@EnableSwagger2
+public class AuthorizationApplicationWithJwtApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AutorizationApplicationWithJwtApplication.class, args);
+        SpringApplication.run(AuthorizationApplicationWithJwtApplication.class, args);
     }
 
 }
