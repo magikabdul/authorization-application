67b5d23acaa07d43615873291b45860fcbe7dae0
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2020-05-20 22:02:52 +0200

    feat: added register form handling

--- frontend/src/components/organisms/RegisterForm.js
+++ frontend/src/components/organisms/RegisterForm.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import { Button, Link, TextField, Typography } from '@material-ui/core';
+import {
+  Button,
+  Link,
+  SvgIcon,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+
+import RegisterIcon from '../../assets/svg/sign-up.svg';
 
 const Wrapper = styled.form`
   position: absolute;
@@ -47,7 +55,22 @@ const ButtonBox = styled.div`
 `;
 
 export function RegisterForm({ setMode }) {
-  console.log(setMode);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = () => {
+    if (password1 !== password2) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
+
   return (
     <Wrapper>
       <Container>
@@ -62,6 +85,8 @@ export function RegisterForm({ setMode }) {
               size='small'
               fullWidth
               required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </InputBox>
           <InputBox>
@@ -71,6 +96,8 @@ export function RegisterForm({ setMode }) {
               size='small'
               fullWidth
               required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
             />
           </InputBox>
         </NameBox>
@@ -81,6 +108,8 @@ export function RegisterForm({ setMode }) {
             size='small'
             fullWidth
             required
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
           />
         </InputBox>
         <InputBox>
@@ -90,6 +119,8 @@ export function RegisterForm({ setMode }) {
             size='small'
             fullWidth
             required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </InputBox>
         <InputBox>
@@ -99,6 +130,10 @@ export function RegisterForm({ setMode }) {
             size='small'
             fullWidth
             required
+            type='password'
+            value={password1}
+            onChange={(e) => setPassword1(e.target.value)}
+            error={error}
           />
         </InputBox>
         <InputBox>
@@ -108,10 +143,24 @@ export function RegisterForm({ setMode }) {
             size='small'
             fullWidth
             required
+            type='password'
+            value={password2}
+            onChange={(e) => setPassword2(e.target.value)}
+            error={error}
           />
         </InputBox>
         <ButtonBox>
-          <Button variant='contained' color='secondary'>
+          <Button
+            variant='contained'
+            color='secondary'
+            startIcon={
+              <SvgIcon viewBox='0 0 100 100'>
+                <path d='M95.266,63H80V47.264C80,47.129,79.483,47,79.35,47H68.105C67.973,47,68,47.129,68,47.264V63H52.188  C52.056,63,52,63.049,52,63.182v11.243C52,74.558,52.056,75,52.188,75H68v15.343C68,90.476,67.973,91,68.105,91H79.35  c0.134,0,0.65-0.524,0.65-0.657V75h15.266C95.401,75,96,74.558,96,74.425V63.182C96,63.049,95.401,63,95.266,63z'></path>
+                <path d='M52.188,77C50.953,77,50,75.66,50,74.425V63.182C50,61.946,50.953,61,52.188,61H66v-9.745  c-3.917-4.638-9.15-8.166-15.157-9.99c6.024-3.312,10.108-9.683,10.108-17.002c0-10.727-8.763-19.422-19.57-19.422  c-10.807,0-19.568,8.696-19.568,19.422c0,7.32,4.082,13.691,10.109,17.002C18.876,45.225,9.4,57.154,9.4,71.261  c0,17.339,14.318,20.323,31.98,20.323c9.903,0,18.753-0.939,24.619-4.819V77H52.188z'></path>
+              </SvgIcon>
+            }
+            onClick={handleSubmit}
+          >
             REGISTER
           </Button>
         </ButtonBox>
